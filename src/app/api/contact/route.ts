ab9bed15089fc9520e4ffc4d4434687c6e818896
Jamoah4203 <justiceamoah4203@gmail.com>
2025-05-03 12:00:56 +0000

    Updated Resend API Key

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -2,7 +2,6 @@
 import { Resend } from 'resend';
 import { NextResponse } from 'next/server';
 
-//re_W4PPEBjb_Hfr9QyRJeF9MkQE5wZ9y8nCr
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
